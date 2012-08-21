e99a8f5a953ca26fcfb44bff5972df4df1d0790f
Egorvah <scsware@yandex.ru>
2012-08-21 13:53:41 +0400

    merge

--- views/product/_form.php
+++ views/product/_form.php
@@ -14,7 +14,15 @@
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
-
+        
+        <div class="row">
+            <?php echo $form->labelEx($model,'categories'); ?>
+            <?php echo $form->listBox($model, 'categories',
+            CHtml::listData(Category::model()->findAll(), 'id', 'title'),
+            array('multiple' => 'multiple')); ?>
+        </div>
+        
+        
 	<div class="row">
 		<?php echo $form->labelEx($model,'title'); ?>
 		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>200)); ?>
@@ -26,6 +34,11 @@
 		<?php echo $form->textArea($model,'short_description',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'short_description'); ?>
 	</div>
+
+
+
+    <?$this->renderPartial('_info', array("model" => $model))?>
+
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
