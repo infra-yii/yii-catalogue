f7f4f4b4c096575bc43369b4d015d53567e28566
Artem Nikonov <ajaks.spb@gmail.com>
2012-08-31 14:28:02 +0400

    Выделил Searh в отдельный контроллер, преписал вьюхи для поиска

---
+++ views/search/_viewCategory.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this CategoryController */
+/* @var $model Category */
+?>
+
+<div class="view">
+
+    <?php echo CHtml::link(CHtml::encode($data->title), array('list', 'id' => $data->id)); ?>
+    <br/>
+
+</div>
