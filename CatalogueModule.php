cfd11dd72fc2cb9fb3dfd32be9bd5a53e1c3339d
alari <name.alari@gmail.com>
2012-09-14 11:33:34 +0400

    i18n for adminGen

--- CatalogueModule.php
+++ CatalogueModule.php
@@ -27,7 +27,7 @@ class CatalogueModule extends CWebModule
      */
     public function adminGenLinks()
     {
-        $menuItems = array('label' => 'Catalogue', 'url' => '', 'items' => array(
+        $menuItems = array('label' => Yii::t("app", 'Catalogue'), 'url' => '', 'items' => array(
             array('url' => array("/catalogue/category/admin"), 'label' => Yii::t("app", "Manage Categories"), 'visible' => !Yii::app()->user->isGuest),
             array('url' => array("/catalogue/product/admin"), 'label' => Yii::t("app", "Manage Product"), 'visible' => !Yii::app()->user->isGuest)
         ),
